c6d93080e9ba0e52d4f04d93ed765b29959c2279
Ballér Pál Krisztián <deadfair666@gmail.com>
2022-03-19 16:56:17 +0100

    2 modules + 2 routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './components/landing/landing.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { CustomerModule } from './customer/customer.module';
+import { EmployeeModule } from './employee/employee.module';
 
 @NgModule({
   declarations: [AppComponent, LandingComponent, PageNotFoundComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CustomerModule,
+    EmployeeModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/employee/employee-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { EmployeeComponent } from './employee.component';
+
+
+const routes: Routes = [
+  {
+    path:'',
+    component:EmployeeComponent,
+    children:[
+      {
+        path:'',
+        component:EmployeeComponent
+      }
+    ]
+  },
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forChild(routes)
+  ],
+  exports: [RouterModule]
+})
+export class EmployeeRoutingModule { }
