cf15a92227eff0e5abe4a62833325a71a0135772
FoAMDFun <foamdfun@gmail.com>
2022-03-19 20:47:18 +0100

    @angular/fire added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'szalloda-app',
+    appId: '1:569196045069:web:3f6e56567c06f8d4154644',
+    storageBucket: 'szalloda-app.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyBEUOmi6iSZ1Y_0UbFwCTaEjvgVUwPKTvM',
+    authDomain: 'szalloda-app.firebaseapp.com',
+    messagingSenderId: '569196045069',
+  },
   production: true
 };
