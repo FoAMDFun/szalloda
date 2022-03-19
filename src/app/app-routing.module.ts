4cdf4302c3c78cb48cd90e46ac81c56fbc24dc94
FoAMDFun <foamdfun@gmail.com>
2022-03-19 11:08:39 +0100

    Landing page added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LandingComponent } from './landing/landing.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: '', component: LandingComponent }, // A kezdőlap;
