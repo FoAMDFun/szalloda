c3819aa72e277b58e4236a4312b9f6b7a2143a34
FoAMDFun <foamdfun@gmail.com>
2022-03-30 22:00:37 +0200

    Reservation add works (Super basic validation only for now)

--- src/app/customer/components/customer-main/customer-main.component.ts
+++ src/app/customer/components/customer-main/customer-main.component.ts
@@ -1,18 +1,27 @@
-import { ThisReceiver } from '@angular/compiler';
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { select, Store } from '@ngrx/store';
 import { Timestamp } from 'firebase/firestore';
-import { Reservation } from 'src/app/models/reservation.model';
+import { Subscription } from 'rxjs';
+import {
+  Reservation,
+  ReservationStatus,
+} from 'src/app/models/reservation.model';
 import {
   addReservation,
   getReservations,
   deleteReservation,
-  changeReservationDate,
 } from 'src/app/store/actions/reservation.action';
 import { ReservationState } from 'src/app/store/reducers/reservation.reducer';
+import { getAuthUserUidSelector } from 'src/app/store/selectors/auth.selector';
 import {
-  getReservationWithFilterSelector,
+  getReservationsSelector,
   isReservationFilterEmpty,
 } from 'src/app/store/selectors/reservation.selector';
 
@@ -21,27 +30,52 @@ import {
   templateUrl: './customer-main.component.html',
   styleUrls: ['./customer-main.component.scss'],
 })
-export class CustomerMainComponent implements OnInit {
+export class CustomerMainComponent implements OnInit, OnDestroy {
   @ViewChild('startdateref', { static: true }) startDateValue?: ElementRef;
   @ViewChild('enddateref', { static: true }) endDateValue?: ElementRef;
-  reservations$ = this.store.pipe(select(getReservationWithFilterSelector));
+  reservations$ = this.store.pipe(select(getReservationsSelector));
   isReservationsEmpty$ = this.store.pipe(select(isReservationFilterEmpty));
-  startDate = new Date('2022-01-01T00:00:00');
+  startDate = new Date();
   endDate = new Date();
-  today = Date.now();
+  today = new Date();
   public reservationForm: FormGroup = new FormGroup({});
+  public roomValues: Number[] = [];
+  private currentUID: string = '';
+  private uidSub: Subscription;
 
   constructor(
     private store: Store<ReservationState>,
     private formBuilder: FormBuilder
   ) {
+    this.uidSub = this.store
+      .pipe(select(getAuthUserUidSelector))
+      .subscribe((uid) => (this.currentUID = uid));
     this.reservationForm = this.formBuilder.group({
-      roomId: ['', [Validators.required]],
-      comments: ['', [Validators.required]],
+      roomId: ['', [Validators.required, Validators.pattern('^[0-9]*$')]],
+      comments: ['', [Validators.maxLength(100)]],
       startDate: ['', [Validators.required]],
       endDate: ['', [Validators.required]],
-      numberOfCustomers: ['', [Validators.required]],
+      numberOfCustomers: [
+        '1',
+        [Validators.required, Validators.pattern('^[1-4]$')],
+      ],
     });
+
+    for (let i = 1; i <= 5; i++) {
+      this.roomValues.push(i);
+    }
+
+    for (let i = 101; i <= 108; i++) {
+      this.roomValues.push(i);
+    }
+
+    for (let i = 201; i <= 208; i++) {
+      this.roomValues.push(i);
+    }
+
+    for (let i = 301; i <= 308; i++) {
+      this.roomValues.push(i);
+    }
   }
 
   ngOnInit(): void {
@@ -57,21 +91,22 @@ export class CustomerMainComponent implements OnInit {
       return btoa(Math.random().toString()).substr(10, 15);
     }
 
-    const dummyReservation: Reservation = {
-      comments: getRandomString(),
-      numberOfCustomers: 1,
-      customerId: getRandomString(),
-      roomId: 'TWXP8xeBuZz1WNO19tmG',
-      startDate: Timestamp.fromDate(new Date()),
-      endDate: Timestamp.fromDate(
-        new Date(
-          new Date().getFullYear(),
-          new Date().getMonth(),
-          new Date().getDate() + 3
-        )
-      ),
-      _id: getRandomString(),
-    };
+    let dummyReservation: Reservation = this.reservationForm.value;
+    console.log(this.reservationForm.value);
+
+    dummyReservation.status = ReservationStatus.UNCONFIRMED;
+    dummyReservation.customerId = this.currentUID;
+    console.log(this.startDate.toString());
+    console.log(this.endDate.toString());
+    dummyReservation.startDate = Timestamp.fromDate(this.startDate);
+    dummyReservation.endDate = Timestamp.fromDate(this.endDate);
+    // comments: getRandomString(),
+    // numberOfCustomers: 1,
+    // customerId: getRandomString(),
+    // roomId: 'TWXP8xeBuZz1WNO19tmG',
+    // status: ReservationStatus.UNCONFIRMED,
+    // _id: this.currentUID,
+
     this.store.dispatch(addReservation(dummyReservation));
   }
 
@@ -81,7 +116,6 @@ export class CustomerMainComponent implements OnInit {
 
   parseDate(dateString: string | null): Date {
     if (dateString) {
-      console.log(dateString);
       const resultDate = new Date(dateString);
       if (
         resultDate.toString() !== 'Invalid Date' &&
@@ -108,12 +142,16 @@ export class CustomerMainComponent implements OnInit {
       this.endDate.setMilliseconds(Date.now());
       console.error(error);
     } finally {
-      this.store.dispatch(
-        changeReservationDate(
-          Timestamp.fromDate(this.startDate),
-          Timestamp.fromDate(this.endDate)
-        )
-      );
+      // this.store.dispatch(
+      //   changeReservationDate(
+      //     Timestamp.fromDate(this.startDate),
+      //     Timestamp.fromDate(this.endDate)
+      //   )
+      // );
     }
+  }
+
+  ngOnDestroy(): void {
+    this.uidSub.unsubscribe();
   }
 }
